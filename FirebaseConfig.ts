835fe413d591970e11a34e8bcdedc87b14e464ac
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2025-02-05 01:08:00 +0530

    test with data

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebasekey";
+import Constants from "expo-constants";
 
 //console.log("🔥 FirebaseConfig.ts has been loaded!", new Error().stack);
 
@@ -10,11 +10,21 @@ const USE_FIREBASE = false; // Change to true when needed
 
 let app, auth, db;
 
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
+  measurementId: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID,
+};
+
 if (USE_FIREBASE) {
   app = initializeApp(firebaseConfig);
-  console.log("🔥 Firebase initialized");
-  auth = getAuth(app);
   db = getFirestore(app);
+  auth = getAuth(app);
+  console.log("🔥 Firebase initialized");
 } else {
   console.log("❌ Firebase is disabled");
 }
