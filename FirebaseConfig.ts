3c9b0f63ed15c90e5312eb9552884440614ff7cc
dananjaya2002 <kavishandananjaya2002@gmail.com>
2025-02-04 19:22:47 +0530

    key separation

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,19 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import firebasekey from "./firebasekey";
 
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAyXvO9_otbNLa0_oW2YDg4fTohsfHybSA",
-  authDomain: "my-app-1-e6897.firebaseapp.com",
-  projectId: "my-app-1-e6897",
-  storageBucket: "my-app-1-e6897.firebasestorage.app",
-  messagingSenderId: "235457140252",
-  appId: "1:235457140252:web:935eabf6e7bf73bb8831d9",
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebasekey);
 
 // Initialize Firebase Authentication
 const auth = getAuth(app);
