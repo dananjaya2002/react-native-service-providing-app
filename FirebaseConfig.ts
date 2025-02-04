0dd15ecaa6c013d3fb8c0ce6058c33beb54a3828
dananjaya2002 <kavishandananjaya2002@gmail.com>
2025-02-04 20:03:56 +0530

    sync

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import firebasekey from "./firebasekey";
+import firebaseConfig from "./firebasekey";
+
 
 
 
 // Initialize Firebase
-const app = initializeApp(firebasekey);
+const app = initializeApp(firebaseConfig);
+console.log("firebase config", firebaseConfig);
 
 // Initialize Firebase Authentication
 const auth = getAuth(app);
